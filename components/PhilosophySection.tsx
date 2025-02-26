24f2c82c1eb730fe04518b323674ade7707932b8
“Edoardo <edoardo.gambacorta@gmail.com>
2025-02-26 14:58:56 +0100

    -

--- components/PhilosophySection.tsx
+++ components/PhilosophySection.tsx
@@ -5,20 +5,26 @@ import CenteredContainer from './CenteredContainer';
 
 export const PhilosophySection: React.FC = () => {
   return (
-<section className="bg-white text-black w-full pt-16 md:pt-20 lg:pt-24 pb-8 md:pb-12 lg:pb-16">
-  <CenteredContainer>
-    <div className="flex flex-col items-start gap-3">
-      <div className="py-2">
-        <TextRevealByWord 
-          text="The AI Revolution is here  We make sure you're always one step ahead." 
-          className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-8 md:mb-12 lg:mb-16"
-          preHighlightedWords={5}
-          textAlign="left"
-        />
-        <Feature />
-      </div>
-    </div>
-  </CenteredContainer>
-</section>
+    <section className="bg-white text-black w-full pt-16 md:pt-20 lg:pt-24 pb-8 md:pb-12 lg:pb-16">
+      <CenteredContainer>
+        <div className="flex flex-col items-start gap-3">
+          <div className="py-2">
+            <TextRevealByWord 
+              text="The AI Revolution is here  We make sure you're always one step ahead." 
+              className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-8 md:mb-12 lg:mb-16"
+              preHighlightedWords={5}
+              textAlign="left"
+            />
+            <Feature />
+            <TextRevealByWord 
+              text="Find out how we can help you." 
+              className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-8 md:mb-12 lg:mb-16"
+              preHighlightedWords={0}
+              textAlign="left"
+            />
+          </div>
+        </div>
+      </CenteredContainer>
+    </section>
   );
 };
