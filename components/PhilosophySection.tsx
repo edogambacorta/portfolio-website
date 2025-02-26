c31a3fe7a806a7d73913ff6dd0e02fcb513bd187
“Edoardo <edoardo.gambacorta@gmail.com>
2025-02-26 17:04:54 +0100

    -

--- components/PhilosophySection.tsx
+++ components/PhilosophySection.tsx
@@ -5,7 +5,7 @@ import CenteredContainer from './CenteredContainer';
 
 export const PhilosophySection: React.FC = () => {
   return (
-    <section className="bg-white text-black w-full pt-16 md:pt-20 lg:pt-24 pb-8 md:pb-12 lg:pb-16">
+    <section className="bg-white text-black w-full pt-16 md:pt-20 lg:pt-24 pb-0 md:pb-0 lg:pb-0">
       <CenteredContainer>
         <div className="flex flex-col items-start gap-3">
           <div className="py-2">
@@ -18,7 +18,7 @@ export const PhilosophySection: React.FC = () => {
             <Feature />
             <TextRevealByWord 
               text="Find out how we can help you." 
-              className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-8 md:mb-12 lg:mb-16"
+              className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-8 md:mb-0 lg:mb-5"
               preHighlightedWords={0}
               textAlign="left"
             />
